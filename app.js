73cb07d98443e1aece1c2446a04a2e70f179e63e
Per-Anders Karlsson <karlsson.peranders@gmail.com>
2019-10-29 15:33:59 +0100

    heroku push

---
+++ app.js
@@ -0,0 +1,6 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080
+const path = require('path')
+app.use(express.static(path.join(__dirname, 'public')))
+app.listen(port, () => console.log('Server started on port: ' + port))
